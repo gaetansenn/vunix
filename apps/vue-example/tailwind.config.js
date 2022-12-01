626f4092afe82e0b0cee403de06c4d03c9287af9
Gaetan SENN <gaetan.senn@gmail.com>
2022-12-01 15:42:00 +0100

    feat(vue-example): use plugin for content injection of core

--- apps/vue-example/tailwind.config.js
+++ apps/vue-example/tailwind.config.js
@@ -1,18 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-const coreDistPath = require.resolve('@vunix/core').replace('index.ts.mjs', '')
-
-console.log(coreDistPath.replace('dist/', 'dist/runtime/components/**/*.{vue,js,ts}'))
-console.log(coreDistPath.replace('dist/', 'dist/runtime/utils/config.ts'))
-
 module.exports = {
-  content: [
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    coreDistPath.replace('dist/', 'dist/runtime/components/**/*.{vue,js,ts}'),
-    coreDistPath.replace('dist/', 'dist/runtime/utils/config.{ts,mjs}')
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [require('@vunix/vue').tailwind],
 }
