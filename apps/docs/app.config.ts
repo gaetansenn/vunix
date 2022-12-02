21f49c92c2a18ac259a6d893e8f947919ee907a2
Gaetan SENN <gaetan.senn@gmail.com>
2022-12-02 17:59:47 +0100

    feat(docs): update index page

--- apps/docs/app.config.ts
+++ apps/docs/app.config.ts
@@ -4,27 +4,13 @@ export default defineAppConfig({
     description: 'An open source ui library for developing HTML/CSS and JS websites on top of tailwind css and compatible with Vue 3 and Nuxt 3',
     image: '/social-card-preview.png',
     socials: {
-      twitter: '@docus_',
-      github: 'nuxt-themes/docus'
+      twitter: '@gaetansenn',
+      github: 'gaetansenn/vunix'
     },
     github: {
       root: 'content',
       edit: true,
       contributors: false
-    },
-    footer: {
-      icons: [
-        {
-          label: 'NuxtJS',
-          href: 'https://nuxtjs.org',
-          icon: 'IconNuxtLabs'
-        },
-        {
-          label: 'Vue Telescope',
-          href: 'https://vuetelescope.com',
-          icon: 'IconVueTelescope'
-        }
-      ]
     }
   }
 })
