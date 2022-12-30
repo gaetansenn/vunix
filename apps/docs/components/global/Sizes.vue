f8a493e4e8a8f74b35147e3599868c4a5a6444bd
Gaetan SENN <gaetan.senn@gmail.com>
2022-12-30 14:54:14 +0100

    feat(docs): add theming section

--- apps/docs/components/global/Sizes.vue
+++ apps/docs/components/global/Sizes.vue
@@ -1,24 +1,25 @@
 <template>
-    <h2 class="text-blue-400 font-medium mb-5">Using size prop</h2>
-    <div class="flex space-x-2">
-        <component v-for="size in sizes" :is="component" :size="size">
-            {{ name }} {{ size }}
-        </component>
-    </div>
+  <h2 class="text-blue-400 font-medium mb-5">Using size prop</h2>
+  <div class="inline-flex items-start space-x-2">
+    <component v-for="size in sizes" :is="component" :size="size">
+      {{ name }} {{ size }}
+    </component>
+  </div>
 </template>
 
 <script setup>
 const props = defineProps({
-    name: {
-        type: String,
-        default: null
-    },
-    component: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    default: null
+  },
+  component: {
+    type: String,
+    required: true
+  }
 })
 
+const { $vunix } = useNuxtApp()
 const name = props.name || props.component
-const sizes = ref(['xs', 'sm', 'md', 'lg'])
+const sizes = computed(() => Object.keys($vunix?.config.Button.sizes || []))
 </script>
