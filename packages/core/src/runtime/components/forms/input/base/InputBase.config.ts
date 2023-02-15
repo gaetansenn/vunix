0aac46e0107cf755a2723efb3c9e8b37c6801003
Gaetan SENN <gaetan.senn@gmail.com>
2023-02-15 22:47:29 +0100

    feat(core): handle leading/trailing from config and emit click

--- packages/core/src/runtime/components/forms/input/base/InputBase.config.ts
+++ packages/core/src/runtime/components/forms/input/base/InputBase.config.ts
@@ -1,15 +1,17 @@
-import type { DefaultConfig, MethodOrObject, MethodOrStringType } from "../../../../utils/config"
+import type { DefaultConfig, IconType, MethodOrObject, MethodOrStringType } from "../../../../utils/config"
 
 export interface InputBaseConfig extends DefaultConfig {
   input: {
     class: MethodOrStringType,
   }
   trailing: {
+    icon?: IconType,
     class: MethodOrStringType,
     sizes?: MethodOrObject,
     size?: MethodOrStringType,
   },
   leading: {
+    icon?: IconType
     class: MethodOrStringType,
     sizes?: MethodOrObject,
     size?: MethodOrStringType,
--- packages/core/src/runtime/components/forms/input/base/InputBase.vue
+++ packages/core/src/runtime/components/forms/input/base/InputBase.vue
@@ -1,28 +1,29 @@
 <template>
   <div
     :class="[config.InputBase.class, config.InputBase.variant, config.InputBase.rounded, config.InputBase.size, { 'focused': focused }]">
-    <div v-if="props.leading || slots.leading" :class="config.InputBase.leading.class">
+    <div v-if="leadingIcon || slots.leading" :class="config.InputBase.leading.class">
       <slot name="leading">
-        <Icon v-if="typeof props.leading === 'string'" :name="props.leading"
-          :size="config.InputBase.leading.size as any" />
-        <component v-else :is="props.leading" :class="[config.InputBase.leading.size]" />
+        <Icon v-if="typeof leadingIcon === 'string'" :name="leadingIcon" :size="(config.InputBase.leading.size as any)"
+          @click="$emit('leadingClick')" />
+        <component v-else :is="props.leading" :class="[config.InputBase.leading.size]" @click="$emit('leadingClick')" />
       </slot>
     </div>
     <input ref="input" v-bind="$attrs" :type="props.type" :value="modelValue" :class="[config.InputBase.input.class]"
       :placeholder="placeholder" @input="$emit('update:modelValue', handleInputChange($event))"
       @blur="$emit('blur', $event)" @focus="$emit('focus', $event)" />
-    <div v-if="props.trailing || slots.trailing" :class="config.InputBase.trailing.class">
+    <div v-if="trailingIcon || slots.trailing" :class="config.InputBase.trailing.class">
       <slot name="trailing">
-        <Icon v-if="typeof props.trailing === 'string'" :name="props.trailing"
-          :size="config.InputBase.trailing.size as any" />
-        <component v-else :is="props.trailing" :class="[config.InputBase.trailing.size]" />
+        <Icon v-if="typeof trailingIcon === 'string'" :name="trailingIcon"
+          :size="(config.InputBase.trailing.size as any)" @click="$emit('trailingClick')" />
+        <component v-else :is="props.trailing" :class="[config.InputBase.trailing.size]"
+          @click="$emit('trailingClick')" />
       </slot>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, getCurrentInstance, inject, ref, useSlots } from 'vue';
+import { defineComponent, getCurrentInstance, inject, ref, useSlots, computed } from 'vue';
 import { useFocus } from '@vueuse/core';
 
 import { useConfig } from '../../../../composables/config';
@@ -30,7 +31,11 @@ import Icon from '../../../icon/Icon.vue'
 import { VunixConfigSymbol } from '../../../../symbols'
 import { injectDefaultValues } from '../../../commons/props';
 import { props as inputProps } from './InputBase.props'
-import type { InputTextConfig } from '../text/InputText.config';
+import type { InputBaseConfig } from './InputBase.config';
+
+interface Config {
+  InputBase: InputBaseConfig
+}
 
 export default defineComponent({
   components: {
@@ -38,7 +43,7 @@ export default defineComponent({
   },
   props: inputProps(),
   inheritAttrs: false,
-  emits: ['update:modelValue', 'focus', 'blur'],
+  emits: ['update:modelValue', 'focus', 'blur', 'trailingClick', 'leadingClick'],
   setup(props) {
     const handleInputChange = (event: Event) => (event.target as HTMLInputElement).value
 
@@ -49,12 +54,22 @@ export default defineComponent({
     // TODO: Check if we keep useFocus 
     const { focused } = useFocus(input)
 
-    const config = useConfig<InputTextConfig>({
+    const config = useConfig<Config>({
       props,
       focused
     }, inject(VunixConfigSymbol))
 
-    return { config, props, slots: useSlots(), handleInputChange, focused, input }
+    const leadingIcon = computed(() => {
+      return props.leading || config.InputBase.leading.icon
+    })
+
+    const trailingIcon = computed(() => {
+      return props.trailing || config.InputBase.trailing.icon
+    })
+
+    console.log(trailingIcon)
+
+    return { config, props, slots: useSlots(), handleInputChange, focused, input, leadingIcon, trailingIcon }
   }
 })
 </script>
