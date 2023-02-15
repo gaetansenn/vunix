2a226748307aa1281ceb1e35cf72480f2e4a4d26
Gaetan SENN <gaetan.senn@gmail.com>
2023-02-15 14:16:58 +0100

    fix(core): remove global T typing

--- packages/core/src/runtime/symbols.ts
+++ packages/core/src/runtime/symbols.ts
@@ -1,7 +1,6 @@
-import type { T } from "vitest/dist/global-e98f203b";
 import type { InjectionKey, UnwrapNestedRefs } from "vue";
 import type { Field } from "./composables/forms/field";
 import type { Config } from "./utils/config";
 
 export const VunixConfigSymbol: InjectionKey<UnwrapNestedRefs<Config>> = Symbol('vunix-config')
-export const VunixFieldSymbol: InjectionKey<Field<T>> = Symbol('vunix-field')
+export const VunixFieldSymbol: InjectionKey<Field<unknown>> = Symbol('vunix-field')
