7d6c9d1ab23c09d7aa4cf7d9d84699e777f63410
Gaetan SENN <gaetan.senn@gmail.com>
2023-02-15 13:42:29 +0100

    build(core): use single script block to avoid build issue

--- packages/core/src/runtime/components/forms/input/base/InputBase.vue
+++ packages/core/src/runtime/components/forms/input/base/InputBase.vue
@@ -21,40 +21,40 @@
   </div>
 </template>
 
-<!-- <script lang="ts">
-export default {
-  inheritAttrs: false
-}
-</script> -->
-
-<script setup lang="ts">
-import { getCurrentInstance, inject, ref, useSlots } from 'vue';
+<script lang="ts">
+import { defineComponent, getCurrentInstance, inject, ref, useSlots } from 'vue';
 import { useFocus } from '@vueuse/core';
-import { useConfig } from '../../../../composables/config';
 
+import { useConfig } from '../../../../composables/config';
 import Icon from '../../../icon/Icon.vue'
 import { VunixConfigSymbol } from '../../../../symbols'
 import { injectDefaultValues } from '../../../commons/props';
 import { props as inputProps } from './InputBase.props'
 import type { InputTextConfig } from '../text/InputText.config';
 
-const props = defineProps(inputProps())
+export default defineComponent({
+  components: {
+    Icon
+  },
+  props: inputProps(),
+  inheritAttrs: false,
+  emits: ['update:modelValue', 'focus', 'blur'],
+  setup(props) {
+    const handleInputChange = (event: Event) => (event.target as HTMLInputElement).value
 
-defineEmits(['update:modelValue', 'focus', 'blur'])
+    // Inject default values
+    injectDefaultValues(getCurrentInstance()?.props, inputProps, inject(VunixConfigSymbol)?.InputBase.defaults)
 
-const handleInputChange = (event: Event) => (event.target as HTMLInputElement).value
+    const input = ref(null)
+    // TODO: Check if we keep useFocus 
+    const { focused } = useFocus(input)
 
-// Inject default values
-injectDefaultValues(getCurrentInstance()?.props, inputProps, inject(VunixConfigSymbol)?.InputBase.defaults)
+    const config = useConfig<InputTextConfig>({
+      props,
+      focused
+    }, inject(VunixConfigSymbol))
 
-const input = ref(null)
-// TODO: Check if we keep useFocus 
-const { focused } = useFocus(input)
-
-const config = useConfig<InputTextConfig>({
-  props,
-  focused
-}, inject(VunixConfigSymbol))
-
-const slots = useSlots()
+    return { config, props, slots: useSlots(), handleInputChange, focused }
+  }
+})
 </script>
