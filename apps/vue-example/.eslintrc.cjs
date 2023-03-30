b132e9d53771264106d7f3d414966433097d8170
Gaetan SENN <gaetan.senn@gmail.com>
2023-03-30 10:55:57 +0200

    build(vue-example): upgrade eslintrc and dependencies

--- apps/vue-example/.eslintrc.cjs
+++ apps/vue-example/.eslintrc.cjs
@@ -6,12 +6,9 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
+    '@vue/eslint-config-typescript'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  },
-  singleQuote: true,
-  trailingComma: 'all',
+  }
 }
