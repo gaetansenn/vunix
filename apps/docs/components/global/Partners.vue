ff6a2d01a6595edd1ec76cf333ab4575dd037b8d
Gaetan SENN <gaetan.senn@gmail.com>
2023-04-04 12:18:45 +0200

    feat(docs): add partner section

---
+++ apps/docs/components/global/Partners.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center -mt-8 lg:-mt-20 mb-12 lg:mb-8">
+    <div class="text-4xl font-bold">Partners</div>
+    <div class="flex items-center justify-center mt-5">
+      <a href="https://www.useradgents.com/">
+        <img alt="Useradgents" class="hidden dark:block" src="/partners/ua_white.svg" width="150">
+        <img alt="Useradgents" class="block dark:hidden" src="/partners/ua_black.svg" width="150">
+      </a>
+    </div>
+  </div>
+</template>
