9254f672742575e01422f3c07e0647d203961389
Gaetan SENN <gaetan.senn@gmail.com>
2023-04-18 17:50:32 +0200

    feat(docs): add transitions section for fade,collapse,config transitions

---
+++ apps/docs/components/global/CollapseTransition.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col">
+    <VCollapseTransition>
+      <div v-show="show">
+        <ContentSlot :use="$slots.default" unwrap="p" />
+      </div>
+    </VCollapseTransition>
+    <VButton @click="show = !show" class="mt-4">Toggle</VButton>
+  </div>
+</template>
+
+<script setup>
+const show = ref(false)
+</script>
---
+++ apps/docs/components/global/FadeTransition.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="inline-flex flex-col gap-4">
+    <VFadeTransition>
+      <div v-show="show">
+        <ContentSlot :use="$slots.default" unwrap="p" />
+      </div>
+    </VFadeTransition>
+    <VButton @click="show = !show">Click to {{ show ? 'hide' : 'show' }}</VButton>
+  </div>
+</template>
+
+<script setup>
+const show = ref(false)
+</script>
