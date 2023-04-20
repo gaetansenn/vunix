d28c5bca8d3439ad6c1e8cd204a6d95982061429
Gaetan SENN <gaetan.senn@gmail.com>
2023-04-20 22:54:10 +0200

    feat(docs): add VCard documentation

---
+++ apps/docs/components/global/DCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <VCard :header="header" :collapse="!!collapse">
+    <template v-if="$slots.header" #header>
+      <ContentSlot :use="$slots.header" unwrap="p" />
+    </template>
+    <ContentSlot :use="$slots.default" unwrap="p" />
+    <template v-if="$slots.footer" #footer>
+      <ContentSlot :use="$slots.footer" unwrap="p" />
+    </template>
+  </VCard>
+</template>
+
+<script setup>
+defineProps({
+  header: {
+    type: String
+  },
+  collapse: {
+    type: String,
+    default: ''
+  }
+})
+</script>
